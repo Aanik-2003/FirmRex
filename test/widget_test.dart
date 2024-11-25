801c90bde3fc8be39fe1736b705aa41548c018bb
Nahakul Thapa <noahthapa777@gmail.com>
2024-11-25 22:20:40 +0545

    this is new

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:demp_app/main.dart';
+import 'package:firm_rex/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
